d456890fedd9ccd7a032f5dc7b7c20e6e557a30f
Javier Leguízamo <javier.leguizamo.ie.tesvg@gmail.com>
2021-05-11 11:46:04 -0500

    Polimorfismo en consola

---
+++ Polimorfismo_2/Polimorfismo_2/Automovil.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: jimmy
+ * Date: 11/05/2021
+ * Time: 11:31 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Polimorfismo_2
+{
+	/// <summary>
+	/// Description of Automovil.
+	/// </summary>
+	public class Automovil
+	{
+		String modelo, marca, tipo;
+		
+		public Automovil(String modelo,
+		                 String marca,
+		                 String tipo)
+		{
+			this.modelo = modelo;
+			this.marca  = marca;
+			this.tipo   = tipo;
+		}
+		
+		public Automovil(String tipo)
+		{
+			this.modelo = "Reciente";
+			this.marca  = "Generica";
+			this.tipo   = tipo;
+		}
+		
+		public void acelerar(int velocidad, int tiempo){
+			Console.WriteLine("Acelerando a " + 
+			                  velocidad + " Km/h " +
+			                  "por " + tiempo + " minutos");
+		}
+		public void acelerar(int velocidad){
+			Console.WriteLine("Acelerando a " + 
+			                  velocidad + " Km/h " +
+			                  "por tiempo indefinido");
+		}
+		
+	}
+}
---
+++ Polimorfismo_2/Polimorfismo_2/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: jimmy
+ * Date: 11/05/2021
+ * Time: 11:31 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Polimorfismo_2
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			
+			Automovil bocho = new Automovil("1234","WV","Aut");
+			Automovil copper = new Automovil("Manual");
+			
+			bocho.acelerar(20,10);
+			copper.acelerar(100);
+			
+			Console.ReadKey(true);
+		}
+	}
+}
